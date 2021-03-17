705530ea048c5717899f57779bf2d5933fe9eed4
v.bazhenov <sleem81@list.ru>
2021-03-17 21:29:48 +0500

    day three

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-const Wer = {
+const Sert = {
     data(){
         return {
             inputText: "",
             arr: []
         }
     },
-    methods: {
-        add(){
-            if(this.inputText != '') {
-                this.arr.push(this.inputText);
-            }
-            this.inputText = '';
-        },
-        remove(index){
-            this.arr.splice(index,1);
-        },
-        lowerCase(val){
-           return val.toLowerCase();
+    watch: {
+        inputText(val){
+            val.length < 14? this.inputText = this.inputText.toUpperCase():"";
         }
     },
-    computed: {
+    methods: {
+        lowerCase(){
+            console.log(this.arr);
+        },
+        add(){
 
-    },
-    watch: {
-        inputText(value){
-            if(value.length < 4) this.inputText = this.inputText.toUpperCase();
         }
     }
 }
 
-Vue.createApp(Wer).mount("#app")
+Vue.createApp(Sert).mount("#app")
