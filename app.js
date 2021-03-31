2f0941c1827d30e6f633a1f38b3e53f29f6f9eb3
v.bazhenov <sleem81@list.ru>
2021-03-31 21:51:27 +0500

    31.03.2021

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const Sert = {
+const Saer = {
     data(){
         return {
-            inputText: "",
-            arr: []
-        }
-    },
-    watch: {
-        inputText(val){
-            val.length < 14? this.inputText = this.inputText.toUpperCase():"";
+            text: "",
+            arr: [1,2,3,4,5,6,7,8],
+            obj: {id: "qwe", id: "wer", id: "ert"}
         }
     },
     methods: {
-        lowerCase(){
-            console.log(this.arr);
+        del_obj(idx){
+            delete this.obj[idx];
         },
-        add(){
-
+        del(idx){
+            this.arr.splice(idx, 1);
+        },
+        test(){
+            this.$refs.test.innerHTML = "test";
+        },
+        add(event){
+            this.arr.unshift(event.target.value);
+            event.target.value='';
+        }
+    },
+    computed: {
+        evens(idx) {
+            return this.arr.filter(id => id % 2===0);
         }
     }
 }
-
-Vue.createApp(Sert).mount("#app")
+Vue.createApp(Saer).mount("#app")
